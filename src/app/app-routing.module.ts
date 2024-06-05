30d368152445316364ce56148ed053f27226cd62
Newton <newtonkagimbi11@gmail.com>
2024-06-05 21:37:29 +0300

    Add feature modules for rooms and header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeesComponent } from './employees/employees.component';
-import { RoomsComponent } from './rooms/rooms.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RoomBookingComponent } from './rooms/room-booking/room-booking.component';
-import { AddRoomComponent } from './rooms/add-room/add-room.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'employees', component: EmployeesComponent},
-  { path: 'rooms', component: RoomsComponent},
-  { path: 'rooms/add', component: AddRoomComponent},
-  { path: 'rooms/:id', component: RoomBookingComponent},
   { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RoomsComponent } from './rooms/rooms.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
-import { HeaderComponent } from './header/header.component';
 import { ContainerComponent } from './container/container.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { APP_CONFIG, APP_CONFIG_TOKEN } from './app_config/appconfig.service';
@@ -21,12 +17,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RoomBookingComponent } from './rooms/room-booking/room-booking.component';
 import { FormsModule } from '@angular/forms';
-import { AddRoomComponent } from './rooms/add-room/add-room.component';
 import { LoginComponent } from './login/login.component';
 import { HoverDirective } from './hover.directive';
 import { EmailvalidatorDirective } from './email_validator/emailvalidator.directive';
+import { RoomsModule } from './rooms/rooms.module';
+import { HeaderModule } from './header/header.module';
 
 function initFactory(initService : InitService) {
   return () => initService.init();
@@ -35,22 +31,16 @@ function initFactory(initService : InitService) {
 @NgModule({
   declarations: [
     AppComponent,
-    RoomsComponent,
-    RoomsListComponent,
-    HeaderComponent,
     ContainerComponent,
     EmployeesComponent,
     NavigationComponent,
     NotFoundComponent,
-    RoomBookingComponent,
-    AddRoomComponent,
     LoginComponent,
     HoverDirective,
     EmailvalidatorDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MatToolbarModule,
@@ -58,7 +48,10 @@ function initFactory(initService : InitService) {
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    FormsModule
+    FormsModule,
+    RoomsModule,
+    HeaderModule,
+    AppRoutingModule
   ],
   providers: [{
     provide: APP_CONFIG_TOKEN,
