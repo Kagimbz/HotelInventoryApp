57880d04e80bae60efee4909fe4c129aedf0f1be
Newton <newtonkagimbi11@gmail.com>
2024-06-16 18:37:35 +0300

    Add reactive form for booking room

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,12 +12,12 @@ const routes: Routes = [
   { path: 'rooms', 
     loadChildren: () => import('./rooms/rooms.module').then((m) => m.RoomsModule),
     // canActivate: [loginGuard],
-    canMatch: [login2Guard]
+    // canMatch: [login2Guard]
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'booking', 
     loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule), 
-    canActivate: [loginGuard]
+    // canActivate: [loginGuard]
   },
   { path: '**', component: NotFoundComponent }
 ];
