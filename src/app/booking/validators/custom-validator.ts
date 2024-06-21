6a338a9932c2aa2c625b3f8d4b32e5a216d3c41b
Newton <newtonkagimbi11@gmail.com>
2024-06-21 22:27:57 +0300

    Add custom validator method for validating checkoutDate against checkinDate

--- src/app/booking/validators/custom-validator.ts
+++ src/app/booking/validators/custom-validator.ts
@@ -1,10 +1,30 @@
-import { AbstractControl } from "@angular/forms";
+import { AbstractControl, FormGroup } from "@angular/forms";
 
-export class CustomValidator{
+export class CustomValidator {
     static validateName(control: AbstractControl) {
         const value = control.value as string;
         if (value.includes('test')) {
             return { invalidName: true }
+        }
+        return null;
+    }
+
+    static validateCharacterPresence(char: string) {
+        return (control: AbstractControl) => {
+            const value = control.value as string;
+            if (value.includes(char)) {
+                return { invalidName: true }
+            }
+            return null;
+        }
+    }
+
+    static validateDate(control: FormGroup) {
+        const checkInDate: any = new Date(control.get('checkInDate')?.value);
+        const checkOutDate: any = new Date(control.get('checkOutDate')?.value);
+        const diff = (checkOutDate - checkInDate)/(1000 * 60 * 60 * 24);
+        if (diff < 0) {
+            return { invalidDate: true }
         }
         return null;
     }
